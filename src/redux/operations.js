b2cc5d7c6badf5e5286b45a3f5c46e7ecf44e9ac
Samcia <samantta689@gmail.com>
2023-12-06 15:22:49 +0100

    build operations

---
+++ src/redux/operations.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL =
+  'https://64b163ec062767bc482624ca.mockapi.io/phonebook/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      console.log(contact);
+      const postResponse = await axios.post('/contacts', contact);
+      return postResponse.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      const deleteResponse = await axios.delete(`/contacts/${id}`);
+      return deleteResponse.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
